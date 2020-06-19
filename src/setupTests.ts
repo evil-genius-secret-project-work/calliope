18d4b0596a62dab8cc9310479e5366028e5460dd
Genius <evilgenius@secretproject.work>
2020-06-19 03:12:08 -0400

    Enforce file headers for license.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,23 @@
+/*
+ Calliope
+ Copyright © 2020 Evil Genius
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License version 3
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import '@testing-library/jest-dom/extend-expect';
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
